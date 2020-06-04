869198f3033311bdf769819764dd0826fc05fa5a
Tom Helmuth <thelmuth@hamilton.edu>
2020-06-04 13:13:23 -0400

    Working version of 99 bot, without state written to disk.

--- ninety_nine.py
+++ ninety_nine.py
@@ -12,34 +12,58 @@ client = discord.ext.commands.Bot(command_prefix = '99')
 with open("ninety_nine.key") as f:
     auth_key = f.read().strip()
 
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+    def __len__(self):
+        return len(self.cards)
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.cards.sort()
+
+    def remove_card(self, card):
+        self.cards.remove(card)
+
 class NinetyNineBot:
 
     def __init__(self):
         self.deck = list(range(100))
         random.shuffle(self.deck)
 
-        self.player_cards = {}
+        self.players = {}
+        self.player_id_list = []
+
+    def add_player(self, id, name):
+        """Adds this player to the game."""
+        self.players[id] = Player(name)
+        self.player_id_list.append(id)
 
     def draw(self, id):
         """Draws a card, adds it to player id's hand, and returns it."""
         card = self.deck.pop()
-
-        self.player_cards[id] = self.player_cards.get(id, []) + [card]
-
+        self.players[id].add_card(card)
         return card
 
     def hand(self, id):
         """Returns hand of player given by id"""
-        self.player_cards[id].sort()
-        return self.player_cards[id]
+        return self.players[id].cards
 
     def play(self, id, card):
         """Plays the card from hand of player id."""
-        self.player_cards[id].remove(card)
+        self.players[id].remove_card(card)
 
     def cards_per_hand(self):
         """Returns a string of the number of cards per hand for each player."""
-        return "not working yet"
+        s = ""
+        for id in self.player_id_list:
+            name = self.players[id].name
+            cards = len(self.players[id])
+            s += "{} has {} cards.\n".format(name, cards)
+        return s[:-1]
 
 
 NNB = NinetyNineBot()
@@ -50,18 +74,25 @@ async def on_ready():
     """ Displayed in the terminal when the bot is logged in. """
     print("Who wants to play The Game of 99?")
 
+@client.command(aliases=['join'])
+async def _99join(ctx):
+    """Adds this player to the game."""
+    NNB.add_player(ctx.message.author.id, ctx.message.author.name)
+    await ctx.send("Welcome to the game, {}".format(ctx.message.author.name))
+    print("Added {} to the game.".format(ctx.message.author.name))
+
 
 @client.command(aliases=['draw'])
 async def _99draw(ctx):
-    """Run with the !99draw command"""
+    """Run with the 99draw command"""
     card = NNB.draw(ctx.message.author.id)
     hand = NNB.hand(ctx.message.author.id)
     await ctx.message.author.send("You draw the card {}, and your hand is {}.".format(card, hand))
     print(ctx.message.author.name, "drew a card.")
 
     # Say number of cards in each hand
-    # message = NNB.cards_per_hand()
-    # await ctx.send(message)
+    message = NNB.cards_per_hand()
+    await ctx.send(message)
 
 @client.command(aliases=['play'])
 async def _99play(ctx, card):
@@ -80,8 +111,8 @@ async def _99play(ctx, card):
         print(ctx.message.author.name, "played the card", card)
 
         # Say number of cards in each hand
-        # message = NNB.cards_per_hand()
-        # await ctx.send(message)
+        message = NNB.cards_per_hand()
+        await ctx.send(message)
 
     else:
         await ctx.send("Sorry {}, you do not have card {} in your hand.".format(ctx.message.author.name, card))
@@ -89,7 +120,7 @@ async def _99play(ctx, card):
 @_99play.error
 async def _99play_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-        await ctx.send("Please tell me which card to play, as in `!99play 29` to play card 29.")
+        await ctx.send("Please tell me which card to play, as in `99play 29` to play card 29.")
 
 
 
